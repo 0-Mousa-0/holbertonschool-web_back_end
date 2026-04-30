c85177fdf329f7dc4531412fd0fe2033a3d0f65a
0-Mousa-0 <mosa.alqarni01@gmail.com>
2026-04-30 17:52:13 +0300

    code style

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-"""Simple helper function
-"""
+"""Simple helper function"""
 from typing import Tuple
 
 
@@ -8,4 +7,4 @@ def index_range(page, page_size) -> tuple[int, int]:
     """Returns a tuple of size two containing a start index and an end index"""
     start = (page - 1) * page_size
     end = start + page_size
-    return (start,  end)
+    return (start, end)

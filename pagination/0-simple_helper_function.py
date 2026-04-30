b278a9cd546583f02f9f8d688fcab34a16f8a25b
0-Mousa-0 <mosa.alqarni01@gmail.com>
2026-04-30 17:41:29 +0300

    give me that page i ask for with all rows

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def index_range(page, page_size):
+    p = page
+    ps = page_size
+    if page == 1:
+        page_size = page_size
+        page = 0
+        return (page, page_size)
+
+    else:
+        page = (p - 1) * ps
+        page_size = p * ps
+        return (page, page_size)

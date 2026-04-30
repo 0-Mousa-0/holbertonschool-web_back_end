730a028ae0616ffbbb9437779c49b6f0bd5a4331
0-Mousa-0 <mosa.alqarni01@gmail.com>
2026-04-30 17:51:26 +0300

    solution thy want

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python3
 """Simple helper function
 """
+from typing import Tuple
 
-def index_range(page, page_size):
-    """a method return page requested and contant rows of it"""
-    p = page
-    ps = page_size
-    if page == 1:
-        page_size = page_size
-        page = 0
-        return (page, page_size)
 
-    else:
-        page = (p - 1) * ps
-        page_size = p * ps
-        return (page, page_size)
+def index_range(page, page_size) -> tuple[int, int]:
+    """Returns a tuple of size two containing a start index and an end index"""
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start,  end)

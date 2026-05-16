cee855e5a7f450cb848d3ca0b7498215fe43685a
Mousa <mosa.alqarni01@gmail.com>
2026-05-16 20:17:55 +0300

    Delete 8-clean_set.js

--- 8-clean_set.js
+++
@@ -1,7 +0,0 @@
-export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string') return '';
-  return [...set]
-    .filter((val) => typeof val === 'string' && val.startsWith(startString))
-    .map((val) => val.slice(startString.length))
-    .join('-');
-}

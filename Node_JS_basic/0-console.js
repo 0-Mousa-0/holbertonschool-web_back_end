ca99b603a32e69e27802ee5e4302dc1a6f95174e
0-Mousa-0 <mosa.alqarni01@gmail.com>
2026-05-17 19:47:27 +0300

    style fixed

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,7 +1,3 @@
-/**
- * Prints a string argument to standard output (STDOUT).
- * @param {string} message - The string message to log.
- */
 function displayMessage(STDOUT) {
   console.log(STDOUT);
 }

72aeadd63723276731326c9b60a2d3f0ce107d20
0-Mousa-0 <mosa.alqarni01@gmail.com>
2026-05-17 18:27:13 +0300

    recieving msg + shows the log console

---
+++ Node_JS_basic/0-console.js
@@ -0,0 +1,8 @@
+/**
+ * Prints a string argument to standard output (STDOUT).
+ * @param {string} message - The string message to log.
+ */
+ function displayMessage(message){
+    console.log(message)
+ }
+ module.exports = displayMessage;

def68a97be5e329d3608f753e25bdada7370a591
0-Mousa-0 <mosa.alqarni01@gmail.com>
2026-05-23 09:55:32 +0300

    fault solution style deleated

--- Node_JS_basic/README.md
+++
@@ -1,1 +0,0 @@
-servers on node.js
--- Node_JS_basic/full_server/controllers/AppController.js
+++
@@ -1,8 +0,0 @@
-export default class AppController {
-  /**
-   * Generates the root home resource response body.
-   */
-  static getHomepage(request, response) {
-    return response.status(200).send('Hello Holberton School!');
-  }
-}
